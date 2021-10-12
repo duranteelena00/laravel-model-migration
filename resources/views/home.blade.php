f20b02336e706b249531dcdab22ee01f6120dc3b
durante.elena00 <durante.elena00@gmail.com>
2021-10-12 18:08:20 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+{{-- Ciao ragazzi,
+per oggi dovrete immaginare un progetto laravel per un'agenzia turistica!
+Preparte il progetto e create un database dove inserire poi una tabella per i pacchetti di viaggio organizzati dall'agenzia.
+Dopo averne definito la struttura tramite una migration, inserite dei dati pseudo-reali tramite phpMyAdmin (bastano due o tre tuple) --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel - model - migration</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
